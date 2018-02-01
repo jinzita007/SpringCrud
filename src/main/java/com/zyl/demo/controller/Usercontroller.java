ffc5bddf87d8bcbcbecf8d6d6d84cc965cbb53bb
天川之约 <woaitianwen@qq.com>
2018-02-02 00:29:16 +0800

    修改文件名

--- src/main/java/com/zyl/demo/controller/Usercontroller.java
+++ src/main/java/com/zyl/demo/controller/Usercontroller.java
@@ -1,4 +1,28 @@
 package com.zyl.demo.controller;
 
-public class Usercontroller {
+import com.zyl.demo.domain.User;
+import com.zyl.demo.mapper.UserMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class UserController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    @RequestMapping("/")
+    public String index() {
+       // User user1 = userMapper.findById(1);
+        User user2 = sqlSession.selectOne("findById","1");
+        //System.out.println(user1);
+        System.out.println(user2);
+
+        return "Greetings from Spring Boot!";
+
+    }
+
 }
