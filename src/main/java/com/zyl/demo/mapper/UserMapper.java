2270283ada0b77ffd455a6b55ae3a01c4ac95d95
天川之约 <woaitianwen@qq.com>
2018-02-01 23:43:54 +0800

    User持久层接口类

--- src/main/java/com/zyl/demo/mapper/UserMapper.java
+++ src/main/java/com/zyl/demo/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package com.zyl.demo.mapper;
 
+import com.zyl.demo.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface UserMapper {
+    @Select("SELECT * FROM users WHERE id = #{id}")
+    User findById(@Param("id") Long id);
 }
